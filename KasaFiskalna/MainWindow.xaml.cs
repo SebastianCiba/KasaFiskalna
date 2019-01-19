7e034632cdf1ccfd73f1e69e10cd7982954dc3c4
Sebastian Ciba <sebastian.ciba@o2.pl>
2019-01-20 00:25:04 +0100

    4

--- KasaFiskalna/MainWindow.xaml.cs
+++ KasaFiskalna/MainWindow.xaml.cs
@@ -1,112 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+
 /*
  *Procedura:
- * 1. wczytanie listy cen z pliku skanera
- * 2. przycisk zatwierdź podlicza sumę do zapłącenia
- * 3a. przycisk "Gotówka" pozwala na wprowadzienie kwoty od klienta w celu wyliczenia reszty
- * 3b. jeśli nie zostanie naciśnięty przcisk "Gotówka" kasa uzna że klient dał wyliczoną kwotę
- * 4. przycisk "OK" drukuje paragon
+ * 1. rozpoczęcie handlu z lientem przyciskiem "Start"
+ * 2. przyciskiem "*skanuje produkt*" zostają dodane do rachunku produkty
+ * (niestety wczytywanie z pliku nie zadziałało i są dodane statycznie 4 produkty)
+ * 3. przycisk zatwierdź podlicza sumę do zapłącenia
+ * 4a. przycisk "Gotówka" pozwala na wprowadzienie kwoty od klienta w celu wyliczenia reszty
+ * 4b. jeśli nie zostanie naciśnięty przcisk "Gotówka" kasa uzna że klient dał wyliczoną kwotę
+ * 5. przycisk "OK" drukuje paragon
  */
 
 namespace KasaFiskalna
 {
     public partial class MainWindow : Window
     {
-        public List<float> bill = new List<float>();
-        public float sum;
-        public float cash;
-        public float change;
-        public string cashString;
-        public bool cashIsClicked;
+        private List<float> BillList = new List<float>();
 
-        
+        private float sum;
+        private float cash;
+        private float change;
+        private string cashString;
+        private bool cashIsClicked;
+        private DateTime localDate;
+        private float sumOfCash;
+        private bool inProgress;
+
+        public class Product
+        {
+            public int number;
+            public string name;
+            public float price;
+
+
+            public Product() {}
+
+            public Product(int number, string name, float price)
+            {
+                this.number = number;
+                this.name = name;
+                this.price = price;
+            }
+        }
+
+        public List<Product> listOfPruducts = new List<Product>();
+
+        public List<Product> ListOfPruducts
+        {
+            get => listOfPruducts;
+            set => listOfPruducts = value;
+        }
 
         public MainWindow()
         {
+            ListOfPruducts.Add(new Product(0, "Keczup", 3.89f));
+            ListOfPruducts.Add(new Product(0, "Masło", 5.23f));
+            ListOfPruducts.Add(new Product(0, "Chleb", 1.99f));
+            ListOfPruducts.Add(new Product(0, "Mleko", 2.20f));
             BillTextBlock = new TextBlock();
-            Screen = new TextBox();
-            Print();
+            Scan();
             InitializeComponent();
         }
 
-        private void FButton_Click(object sender, RoutedEventArgs e)
+
+        private void Start_Click(object sender, RoutedEventArgs e)
         {
-            //BillTextBlock.Text += "duba";
-            //Screen.Text += "duba";
-
-            //var culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
-            //culture.NumberFormat.NumberDecimalSeparator = ".";
-            //float number = float.Parse("0.54", culture);
-            //float n = 5;
-            //Screen.Text += n;
-            //Screen.Text += float.Parse("0,99");
-            ////BillTextBlock.Text += float.Parse("0.99");
-            //Screen.Text += float.Parse("99");
-            ////BillTextBlock.Text += float.Parse("0.99");
-            ////BillTextBlock.Text += float.Parse("0,99f");
-            //Screen.Text += number;
-            //BillTextBlock.Text += number;
-            //BillTextBlock.Text += Environment.NewLine;
-
-
-
-            //for (int i = 0; i < bill.Count; i++)
-            //{
-            //    BillTextBlock.Text += bill[i];
-            //    BillTextBlock.Text += Environment.NewLine;
-            //    sum += bill[i];
-            //}
-
+            inProgress = true;
+            BillList.Clear();
+            //header
+            BillTextBlock.Text = "";
+            BillTextBlock.Text += "Sklep Branżowy";
+            BillTextBlock.Text += Environment.NewLine;
+            BillTextBlock.Text += "20-234 Lublin ul. Bursztynowa 2";
+            BillTextBlock.Text += Environment.NewLine;
+            BillTextBlock.Text += localDate = DateTime.Now;
+            BillTextBlock.Text += Environment.NewLine;
+            BillTextBlock.Text += "PARAGON FISKALNY";
+            BillTextBlock.Text += Environment.NewLine;
         }
 
-
-        private void Print()
+        private void Scan()
         {
 
+            //BillTextBlock.Text += "błąd odczytu0";
+            //try
+            //{
+            //    BillTextBlock.Text += "błąd odczytu1";
+            //    using (StreamReader reader = new StreamReader(@"TenisStolowy.csv"))
+            //    {
+            //        BillTextBlock.Text += "błąd odczytu2";
+            //        if (File.Exists(@"TenisStolowy.csv"))
+            //        {
+            //            BillTextBlock.Text += "błąd odczytu4";
+            //            var lines = File.ReadAllLines(@"TenisStolowy.csv", Encoding.UTF8);
+            //            BillTextBlock.Text += "błąd odczytu5";
+            //            float tempPrice;
+            //            int i = 0;
+            //            while ((lines[i] = reader.ReadLine()) != null)
+            //            {
+            //                var actualLine = lines[i].Split(';');
+            //                var culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            //                culture.NumberFormat.NumberDecimalSeparator = ".";
+            //                tempPrice = float.Parse(actualLine[2], culture);
 
-            try
-            {
-                using (StreamReader reader = new StreamReader(@"Skaner/Start.txt"))
-                {
-                    string line;
+            //                listOfPruducts.Add(new Product (int.Parse(actualLine[0]), actualLine[1], tempPrice));
+            //                BillTextBlock.Text += "błąd odczytu6";
+            //                i++;
+            //            }
+            //        }
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    BillTextBlock.Text += "błąd odczytu7";
+            //}
 
-                    while ((line = reader.ReadLine()) != null)
-                    {
+            
+        }
 
-                        bill.Add(float.Parse(line));
-                        BillTextBlock.Text += line;
-                        BillTextBlock.Text += Environment.NewLine;
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                BillTextBlock.Text += "błąd odczytu";
-                BillTextBlock.Text += Environment.NewLine;
-            }
-
-            foreach (float price in bill)
-            {
-                BillTextBlock.Text += price;
-                BillTextBlock.Text += Environment.NewLine;
-                sum += price;
-            }
+        private void Skan_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(0, 3);
+            BillTextBlock.Text += String.Format( "{0}    {1}", listOfPruducts[i].name, listOfPruducts[i].price);
+            BillList.Add(listOfPruducts[i].price);
+            BillTextBlock.Text += Environment.NewLine;
         }
 
         private void accept_Click(object sender, RoutedEventArgs e)
         {
+            for (var i = 0; i < BillList.Count; i++)
+            {
+                var price = BillList[i];
+                BillList[i] += price;
+                sum += price;
+            }
+
             Screen.Text += "Suma: " + sum;
+            Screen.Text += Environment.NewLine;
+            sumOfCash += sum;
         }
 
         private void OKButton_Click(object sender, RoutedEventArgs e)
         {
-            Screen.Text = "";
             if (cashIsClicked)
             {
                 try
@@ -119,9 +162,6 @@ namespace KasaFiskalna
                     BillTextBlock.Text += sum;
                 }
 
-
-                Screen.Text += "Suma: " + sum;
-                Screen.Text += Environment.NewLine;
                 Screen.Text += "Gotówka: " + cash;
                 Screen.Text += Environment.NewLine;
                 Screen.Text += "-----------";
@@ -131,8 +171,6 @@ namespace KasaFiskalna
 
             else
             {
-                Screen.Text += "Suma: " + sum;
-                Screen.Text += Environment.NewLine;
                 Screen.Text += "Gotówka: " + sum;
                 Screen.Text += Environment.NewLine;
                 Screen.Text += "-----------";
@@ -141,7 +179,17 @@ namespace KasaFiskalna
             }
 
             cashIsClicked = false;
+            PrintBill();
+            inProgress = false;
+        }
 
+        private void PrintBill()
+        { 
+            BillTextBlock.Text += "Suma PLN: " + sum;
+            BillTextBlock.Text += Environment.NewLine;
+            BillTextBlock.Text += "Kasa 3 Kasjer nr 0";
+            BillTextBlock.Text += Environment.NewLine;
+            BillTextBlock.Text += "Dziękujemy i zapraszamy ponownie";
         }
 
         private void Screen_TextChanged(object sender, TextChangedEventArgs e)
@@ -226,6 +274,19 @@ namespace KasaFiskalna
                 cashString += ".";
         }
 
-        
+        private void SumButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (inProgress != true)
+            {
+                BillTextBlock.Text = "Suma pieniędzy w kasetce";
+                BillTextBlock.Text += Environment.NewLine;
+                BillTextBlock.Text += sumOfCash;
+            }
+        }
+
+        private void FButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
