12f3d7ff5aad657e9a687a5e92ee37c1daabe928
Sebastian Ciba <sebastian.ciba@o2.pl>
2019-01-19 13:06:57 +0100

    1

--- KasaFiskalna/MainWindow.xaml.cs
+++ KasaFiskalna/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,36 @@ namespace KasaFiskalna
     {
         public MainWindow()
         {
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(("Start.txt"), true))
+                {
+                    writer.WriteLine("Start information");
+                }
+
+                using (StreamReader reader = new StreamReader("Start.txt"))
+                {
+                    string line;
+
+                    while((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("No paper");
+                Console.WriteLine(e.Message);
+            }
+
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
